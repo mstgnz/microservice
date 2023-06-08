97e137e423a42b580472353b9c5877799c350188
mstgnz <mesutgenez@hotmail.com>
2023-06-08 23:04:42 +0300

    login and register handler

--- auth/handler/auth.go
+++ auth/handler/auth.go
@@ -1,9 +1,13 @@
 package handler
 
 import (
+	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/mstgnz/microservice/config"
+	"github.com/mstgnz/microservice/dto"
+	"github.com/mstgnz/microservice/entity"
 	"github.com/mstgnz/microservice/service"
 )
 
@@ -23,45 +27,39 @@ func AuthHandler(authService service.IAuthService) IAuthHandler {
 }
 
 func (c *authHandler) Login(w http.ResponseWriter, r *http.Request) {
-	_ = config.WriteJSON(w, 200, config.Response{Status: true, Message: "Login"})
-	/*var loginDTO dto.LoginDTO
-	errDTO := ctx.ShouldBind(&loginDTO)
+	var loginDTO dto.LoginDTO
+	errDTO := config.ReadJSON(w, r, &loginDTO)
 	if errDTO != nil {
-		response := helper.BuildErrorResponse("Failed to process request", errDTO.Error(), helper.EmptyObj{})
-		ctx.AbortWithStatusJSON(http.StatusBadRequest, response)
+		_ = config.WriteJSON(w, http.StatusBadRequest, config.Response{Status: false, Message: "Failed to process request", Error: errDTO.Error()})
 		return
 	}
 	authResult := c.authService.VerifyCredential(loginDTO.Email, loginDTO.Password)
 	if v, ok := authResult.(entity.User); ok {
-		generatedToken := c.jwtService.GenerateToken(strconv.FormatUint(v.ID, 10))
+		generatedToken := config.GenerateToken(strconv.FormatUint(v.ID, 10))
 		v.Token = generatedToken
-		response := helper.BuildResponse(true, "OK!", v)
-		ctx.JSON(http.StatusOK, response)
+		_ = config.WriteJSON(w, http.StatusOK, config.Response{Status: true, Message: "Login successful", Data: generatedToken})
 		return
 	}
-	response := helper.BuildErrorResponse("Please check again your credential", "Invalid Credential", helper.EmptyObj{})
-	ctx.AbortWithStatusJSON(http.StatusUnauthorized, response)*/
+	_ = config.WriteJSON(w, http.StatusUnauthorized, config.Response{Status: false, Message: "Invalid credential", Error: errDTO.Error()})
+	_ = config.WriteJSON(w, http.StatusUnauthorized, config.Response{Status: false, Message: "dont see me", Error: errDTO.Error()})
+
+	log.Printf("bunu yazmaması lazım ama hadi bakalım")
 }
 
 func (c *authHandler) Register(w http.ResponseWriter, r *http.Request) {
-	_ = config.WriteJSON(w, 200, config.Response{Status: true, Message: "Register"})
-
-	/*var registerDTO dto.RegisterDTO
-	errDTO := ctx.ShouldBind(&registerDTO)
+	var registerDTO dto.RegisterDTO
+	errDTO := config.ReadJSON(w, r, &registerDTO)
 	if errDTO != nil {
-		response := helper.BuildErrorResponse("Failed to process request", errDTO.Error(), helper.EmptyObj{})
-		ctx.AbortWithStatusJSON(http.StatusBadRequest, response)
+		_ = config.WriteJSON(w, http.StatusBadRequest, config.Response{Status: false, Message: "Failed to process request", Error: errDTO.Error()})
 		return
 	}
-
 	if !c.authService.IsDuplicateEmail(registerDTO.Email) {
-		response := helper.BuildErrorResponse("Failed to process request", "Duplicate email", helper.EmptyObj{})
-		ctx.JSON(http.StatusConflict, response)
+		_ = config.WriteJSON(w, http.StatusConflict, config.Response{Status: false, Message: "Email already exists", Error: errDTO.Error()})
+		return
 	} else {
 		createdUser := c.authService.CreateUser(registerDTO)
-		token := c.jwtService.GenerateToken(strconv.FormatUint(createdUser.ID, 10))
-		createdUser.Token = token
-		response := helper.BuildResponse(true, "OK!", createdUser)
-		ctx.JSON(http.StatusCreated, response)
-	}*/
+		token := config.GenerateToken(strconv.FormatUint(createdUser.ID, 10))
+		_ = config.WriteJSON(w, http.StatusCreated, config.Response{Status: true, Message: "Register successful", Data: token})
+		return
+	}
 }
