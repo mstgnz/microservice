73afa911e95e709efcfb7cd4315185ebcc889d4a
mstgnz <mesutgenez@hotmail.com>
2023-06-09 17:24:45 +0300

    model fix

--- auth/entity/user.go
+++ auth/entity/user.go
@@ -1,18 +1,5 @@
 package entity
 
-import (
-	"time"
-
-	"gorm.io/gorm"
-)
-
-type GormModel struct {
-	ID        uint           `gorm:"primaryKey" json:"id"`
-	CreatedAt time.Time      `json:"created_at"`
-	UpdatedAt time.Time      `json:"updated_at"`
-	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
-}
-
 // User Entity
 type User struct {
 	GormModel
