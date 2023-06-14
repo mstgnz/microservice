7783ca9a9e606d4fe6d91bbd2e772d6b17f97cce
mstgnz <mesutgenez@hotmail.com>
2023-06-14 16:48:54 +0300

    validate

---
+++ blog-api/config/validate.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/go-playground/validator/v10"
+)
+
+func Validate(structure interface{}) error {
+	var validate *validator.Validate
+	validate = validator.New()
+	var errStr string
+	var errSlc []error
+	// returns nil or ValidationErrors ( []FieldError )
+	err := validate.Struct(structure)
+	if err != nil {
+		// this check is only needed when your code could produce
+		// an invalid value for validation such as interface with nil
+		// value most including myself do not usually have code like this.
+		if _, ok := err.(*validator.InvalidValidationError); ok {
+			errStr = err.Error()
+		}
+		for _, err := range err.(validator.ValidationErrors) {
+			errStr = fmt.Sprintf("%s %s %s %s", err.Tag(), err.Param(), err.Field(), err.Type().String())
+			errSlc = append(errSlc, errors.New(errStr))
+			errStr = ""
+		}
+		// from here you can create your own error messages in whatever language you wish
+		return errors.Join(errSlc...)
+	}
+	return nil
+}
